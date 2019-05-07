0fd643517df9c53259b733296914390104e7c983
jalemy <jalemy.xps@gmail.com>
2019-05-07 18:49:35 +0900

    記法を統一

--- vscode/setting.sh
+++ vscode/setting.sh
@@ -7,8 +7,8 @@ VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
 # 既にVSCodeのsettings.jsonが存在したら削除して、シンボリックリンクを貼る
 rm "$VSCODE_SETTING_DIR/settings.json"
-ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+ln -s "$SCRIPT_DIR/settings.json" "$VSCODE_SETTING_DIR/settings.json"
 
 # 既にVSCodeのkeybindings.jsonが存在したら削除して、シンボリックリンクを貼る
 rm "$VSCODE_SETTING_DIR/keybindings.json"
-ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+ln -s "$SCRIPT_DIR/keybindings.json" "$VSCODE_SETTING_DIR/keybindings.json"
